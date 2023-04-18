b1c334b197f775d4ba705f7fcd154ecd0b04979c
Артём Макаров <arntengu@gmail.com>
2023-04-18 17:16:47 +0300

    skills

--- src/js/script.js
+++ src/js/script.js
@@ -9,3 +9,10 @@ hamburger.addEventListener('click', () => {
 close.addEventListener('click', () => {
     menu.classList.remove('active');
 });
+
+const counters = document.querySelectorAll('.skills__progress-scale'),
+      lines = document.querySelectorAll('.skills__progress-line span'); 
+
+counters.forEach((item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
